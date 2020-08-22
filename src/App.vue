70ad8ebbd6473d488fb0dbc82428326804cd71c6
Charles Obeng <charlesobeng000@gmail.com>
2020-08-22 21:40:10 +0000

    project set up

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app dark color="#01101A">
+      <div class="d-flex align-center">
+        <v-btn text x-large>
+          SHUGASPOT.COM
+        </v-btn>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text>
+        News
+      </v-btn>
+      <v-btn text>
+        Reveiews
+      </v-btn>
+      <v-btn text>
+        Interviews
+      </v-btn>
+      <v-btn text>
+        Celebspot
+      </v-btn>
+      <v-btn text>
+        TVS
+      </v-btn>
+      <v-btn text>
+        Gallery/photos
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-facebook</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-instagram</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-alert color="#1F84AB" dark dense>
+        <div class="d-flex justify-space-between">
+          <v-btn text>GET ALL YOUR MOVIE UPDATES & MORE!</v-btn>
+          <v-btn class="black--text" color="white">
+            <v-icon left dark>mdi-youtube-subscription</v-icon>
+            SUBSCRIBE TO OUR YOUTUBE
+          </v-btn>
+          <v-btn text>
+            <v-icon left dark>mdi-trophy-award</v-icon>
+            TELLY SHOW AWARDS
+          </v-btn>
+        </div>
+      </v-alert>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,286 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <v-row justify="space-between" no-gutters>
+      <v-col cols="12" sm="6" md="3" xs="12">
+        <v-card outlined>
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-card outlined>
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-card outlined>
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="6" md="3">
+        <v-card outlined>
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+          >
+            <v-card-title>Top 10 Australian beaches</v-card-title>
+          </v-img>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <!-- <div class="d-flex justify-space-between">
+      
+      <v-card max-width="410" outlined>
+        <v-img
+          class="white--text align-end"
+          height="250px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+          <v-card-title>Top 10 Australian beaches</v-card-title>
+        </v-img>
+      </v-card>
+      <v-card max-width="410" outlined>
+        <v-img
+          class="white--text align-end"
+          height="250px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+          <v-card-title>Top 10 Australian beaches</v-card-title>
+        </v-img>
+      </v-card>
+      <v-card max-width="410" outlined>
+        <v-img
+          class="white--text align-end"
+          height="250px"
+          src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        >
+          <v-card-title>Top 10 Australian beaches</v-card-title>
+        </v-img>
+      </v-card>
+    </div> -->
+    <div style="text-align:center;" class="mt-5">
+      <img src="../assets/adds.jpg" alt="" />
+    </div>
+    <div>
+      <v-container>
+        <v-row justify="space-between">
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <v-card outlined>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+              <v-card-text>
+                <v-chip class="ma-2" label color="#D1ECF6">News</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">Read</v-chip>
+              </v-card-text>
+              <v-card-title>
+                Academy Launches New Conversation Series On Race And Gender
+                Equity in Filmmaking “Academy Dialogues: It Starts With Us”
+              </v-card-title>
+
+              <v-card-subtitle>
+                Conversations about race, ethnicity, gender, history,
+                opportunity and the art of filmmaking
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <v-card outlined>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+              <v-card-text>
+                <v-chip class="ma-2" label color="#D1ECF6">Interviews</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">News</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">Read</v-chip>
+              </v-card-text>
+              <v-card-title>
+                Stan Shaw Talks New Horror/Western Film ‘The Pale Door’
+              </v-card-title>
+
+              <v-card-subtitle>
+                RLJE Films and Shudder will release the horror / Western THE
+                PALE DOOR in theaters and On Demand and Digital on August 21,
+                2020.
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <img src="../assets/filmad.jpg" alt="ads" height="400px;" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+
+    <!-- Editors Pick -->
+    <div>
+      <v-container>
+        <span class="category_span">
+          Editors Picks
+        </span>
+        <v-row justify="space-between">
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <v-card outlined>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+              <v-card-text>
+                <v-chip class="ma-2" label color="#D1ECF6">News</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">Read</v-chip>
+              </v-card-text>
+              <v-card-title>
+                Michelle Buteau Lands First Hour-Long Comedy Special on Netflix
+              </v-card-title>
+
+              <v-card-subtitle>
+                Conversations about race, ethnicity, gender, history,
+                opportunity and the art of filmmaking
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <v-card outlined>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+              <v-card-text>
+                <v-chip class="ma-2" label color="#D1ECF6">Interviews</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">News</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">Read</v-chip>
+              </v-card-text>
+              <v-card-title>
+                Exclusive: Filmmaker Chyna Robinson Talks No Ordinary Love,
+                Relationships, and Survival
+              </v-card-title>
+
+              <v-card-subtitle>
+                RLJE Films and Shudder will release the horror / Western THE
+                PALE DOOR in theaters and On Demand and Digital on August 21,
+                2020.
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <v-card outlined>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+              <v-card-text>
+                <v-chip class="ma-2" label color="#D1ECF6">Interviews</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">News</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">Read</v-chip>
+              </v-card-text>
+              <v-card-title>
+                Stan Shaw Talks New Horror/Western Film ‘The Pale Door’
+              </v-card-title>
+
+              <v-card-subtitle>
+                RLJE Films and Shudder will release the horror / Western THE
+                PALE DOOR in theaters and On Demand and Digital on August 21,
+                2020.
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+        </v-row>
+
+        <v-row justify="space-between">
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <v-card outlined>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+              <v-card-text>
+                <v-chip class="ma-2" label color="#D1ECF6">News</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">Read</v-chip>
+              </v-card-text>
+              <v-card-title>
+                Michelle Buteau Lands First Hour-Long Comedy Special on Netflix
+              </v-card-title>
+
+              <v-card-subtitle>
+                Conversations about race, ethnicity, gender, history,
+                opportunity and the art of filmmaking
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <v-card outlined>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+              <v-card-text>
+                <v-chip class="ma-2" label color="#D1ECF6">Interviews</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">News</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">Read</v-chip>
+              </v-card-text>
+              <v-card-title>
+                Exclusive: Filmmaker Chyna Robinson Talks No Ordinary Love,
+                Relationships, and Survival
+              </v-card-title>
+
+              <v-card-subtitle>
+                RLJE Films and Shudder will release the horror / Western THE
+                PALE DOOR in theaters and On Demand and Digital on August 21,
+                2020.
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+          <v-col cols="12" sm="6" md="4" xs="12">
+            <v-card outlined>
+              <v-img
+                src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+                height="200px"
+              ></v-img>
+              <v-card-text>
+                <v-chip class="ma-2" label color="#D1ECF6">Interviews</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">News</v-chip>
+                <v-chip class="ma-2" label color="#D1ECF6">Read</v-chip>
+              </v-card-text>
+              <v-card-title>
+                Stan Shaw Talks New Horror/Western Film ‘The Pale Door’
+              </v-card-title>
+
+              <v-card-subtitle>
+                RLJE Films and Shudder will release the horror / Western THE
+                PALE DOOR in theaters and On Demand and Digital on August 21,
+                2020.
+              </v-card-subtitle>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+  name: 'Home',
 };
 </script>
